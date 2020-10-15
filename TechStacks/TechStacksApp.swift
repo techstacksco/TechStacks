01d55f0f1d32b5c6fb26ecbedf07ea4e1dd6b82e
techstacksco <marc@techstacks.co>
2020-10-15 14:36:34 +0100

    first commit

---
+++ TechStacks/TechStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechStacksApp.swift
+//  TechStacks
+//
+//  Created by Marc Boanas on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TechStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
